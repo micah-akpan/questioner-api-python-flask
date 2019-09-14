8d87cdcec2b79227d773a2f256d90719e467513d
Micah Akpan <mickey4sure2k7@gmail.com>
2019-09-14 21:12:06 +0100

    chore: setup dev environment

---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from app import app
+from flask import jsonify
+
+@app.route('/', methods=['GET'])
+@app.route('/api/v1', methods=['GET'])
+def home():
+  return jsonify({ 'message': 'Welcome to Questioner API v1', 'success': True  })
